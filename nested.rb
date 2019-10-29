a453fb861924098b2cec479f5135bbd19aa8e857
Paul O <paul.orawiec@gmail.com>
2019-10-29 08:16:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,13 +79,9 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        },
-        :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
         }
      }
-
+programmer_hash
     
 end
 
